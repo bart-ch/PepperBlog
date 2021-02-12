85e6be5d5137a0857cfa98a4411087e7aaf61e79
Bartosz Chodyła <bartoszchodyla@gmail.com>
2021-02-12 20:07:06 +0100

    Create articles backend

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article has been successfully created.'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article has been successfully deleted.'
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'The article has been successfully updated.'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+end
