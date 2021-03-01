172142ff45afc6a6f005ba8d541c7f45967717a2
Bartosz Chodyła <bartoszchodyla@gmail.com>
2021-03-01 20:55:15 +0100

    Add password to user

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :articles
+  
   before_save { self.email = email.downcase }
+ 
   validates :username, presence: true, 
             uniqueness: true, 
             length: { minimum:3, maximum:25 }
@@ -8,4 +10,6 @@ class User < ApplicationRecord
             length: { maximum:105 },
             uniqueness: {case_sensitivity: false}, 
             format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password 
+
 end
