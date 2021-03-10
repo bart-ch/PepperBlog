6c3074fcb6f26acfe1f1186c6cb5af6e8b4fd654
Bartosz Chodyła <bartoszchodyla@gmail.com>
2021-03-10 22:47:12 +0100

    Add admin user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.all
@@ -13,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -36,6 +38,19 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+     @user.destroy
+     if @user == current_user
+      flash[:notice] = 'Your account and all your articles have been delted'
+      session[:user_id] = nil
+      redirect_to root_path
+     else
+      flash[:notice] = 'User and all articles created by user have been successfully deleted'
+      redirect_to users_path
+     end
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
@@ -46,9 +61,17 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    unless current_user = @user || current_user.admin?
       flash[:notice] = "You can only edit your own account"
       redirect_to root_path
     end
   end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:notice] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210301194907) do
+ActiveRecord::Schema.define(version: 20210310203131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20210301194907) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
