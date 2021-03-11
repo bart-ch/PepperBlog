941f47ce5866f52557850347838db7524ff650c8
Bartosz Chodyła <bartoszchodyla@gmail.com>
2021-03-11 01:51:33 +0100

    Add tests for category validations

---
+++ test/models/category_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: "Example")
+  end
+
+  test "category name be not be present" do
+    assert @category.valid?
+  end
+
+  test "category should be unique" do
+    @category.save
+    second_category = Category.new(name: "Example")
+    assert_not second_category.valid?
+  end
+
+  test "category should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "category should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+end
